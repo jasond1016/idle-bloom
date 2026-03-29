377f5e5f2dc7ad61bd1b594942c06a1db2d5a3b6
atdt <jasond1016@gmail.com>
2026-03-29 20:12:24 +0800

    Enable animated GIF playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("io.coil-kt:coil:2.7.0")
+    implementation("io.coil-kt:coil-gif:2.7.0")
 }
